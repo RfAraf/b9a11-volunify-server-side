ca9e49ba2eccdb8a2804572190fe0daf7d221638
rfaraf <mohsinarfat53@gamil.com>
2024-05-12 12:42:39 +0600

    created volunteer post

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
 const express = require("express");
 const cors = require("cors");
+require("dotenv").config();
 const { MongoClient, ServerApiVersion } = require("mongodb");
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.0xqywot.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
 const app = express();
 const port = process.env.PORT || 5000;
 
+const corsOptions = {
+  origin: ["http://localhost:5173", "http://localhost:5174"],
+  credentials: true,
+  optionSuccessStatus: 200,
+};
+
 // middlewares
-app.use(cors());
+app.use(cors(corsOptions));
 app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.0xqywot.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -22,6 +31,24 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     // await client.connect();
+
+    const volunteerNeedCollection = client
+      .db("volunteersDB")
+      .collection("volunteerNeed");
+
+    // get all volunteer post
+    app.get("/volunteers", async (req, res) => {
+      const result = await volunteerNeedCollection.find().toArray();
+      res.send(result);
+    });
+
+    // create a volunteer post
+    app.post("/volunteers", async (req, res) => {
+      const volunteerPost = req.body;
+      console.log("new volunteerPost", volunteerPost);
+      const result = await volunteerNeedCollection.insertOne(volunteerPost);
+      res.send(result);
+    });
 
     // Send a ping to confirm a successful connection
     // await client.db("admin").command({ ping: 1 });
